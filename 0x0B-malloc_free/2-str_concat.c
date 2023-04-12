9d3b638c2e5ff143e074a62f788a5c48124828cf
kingofhartz <chimaokorafor@yahoo.com>
2023-04-11 18:34:19 -0700

    test 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,14 +1,29 @@
 #include "main.h"
 		
 #include <stdlib.h>
-
+		
+/**
+		
+ * str_concat - get ends of input and add together for size
+		
+ * @s1: input one to concat
+		
+ * @s2: input two to concat
+		
+ * Return: concat of s1 and s2
+		
+ */
+		
 char *str_concat(char *s1, char *s2)
 		
-{		
+{
+		
 	char *conct;
 		
 	int i, ci;
+		
 
+		
 	if (s1 == NULL)
 		
 		s1 = "";
@@ -16,6 +31,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		
 		s2 = "";
+		
+
 		
 		i = ci = 0;
 		
@@ -28,7 +45,9 @@ char *str_concat(char *s1, char *s2)
 		ci++;
 		
 	conct = malloc(sizeof(char) * (i + ci + 1));
-			
+		
+
+		
 	if (conct == NULL)
 		
 		return (NULL);
@@ -43,8 +62,10 @@ char *str_concat(char *s1, char *s2)
 		
 		i++;
 		
-	}	
-	
+	}
+		
+
+		
 	while (s2[ci] != '\0')
 		
 	{
@@ -60,4 +81,3 @@ char *str_concat(char *s1, char *s2)
 	return (conct);
 		
 }
-

6f45ef389a093b58413835edf7485df3f76f341c
kingofhartz <chimaokorafor@yahoo.com>
2023-04-18 18:29:28 -0700

    tr

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -26,3 +26,4 @@ void free_dog(dog_t *d)
                 free(d);
 
         }
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,55 +3,62 @@
 #define DOG_H
 
 
-/**
 
- * struct dog - a dog's basic info
+        /**
 
- * @name: First member
+         * struct dog - a dog's basic info
 
- * @age: Second member
+         * @name: First member
 
- * @owner: Third member
+         * @age: Second member
 
- *
+         * @owner: Third member
 
- * Description: Longer description
+         *
 
- */
+         * Description: Longer description
 
-struct dog
+         */
 
-{
+        struct dog
 
-        char *name;
+        {
 
-        float age;
+                char *name;
 
-        char *owner;
+                float age;
 
-};
+                char *owner;
 
-
-/**
-
- * dog_t - typedef for struct dog
-
- */
-
-typedef struct dog dog_t;
+        };
 
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
-void print_dog(struct dog *d);
+        /**
 
-dog_t *new_dog(char *name, float age, char *owner);
+         * dog_t - typedef for struct dog
 
-void free_dog(dog_t *d);
+         */
 
-char *_strcpy(char *dest, char *src);
-
-int _strlen(char *s);
+        typedef struct dog dog_t;
 
 
-#endif
+
+        void init_dog(struct dog *d, char *name, float age, char *owner);
+
+        void print_dog(struct dog *d);
+
+        dog_t *new_dog(char *name, float age, char *owner);
+
+        void free_dog(dog_t *d);
+
+        char *_strcpy(char *dest, char *src);
+
+        int _strlen(char *s);
+
+
+
+        #endif
+
+
+
